91e0635cf0da8b83bacfd1df8795a2b11b0845e9
Anton <antonkruger@projectmatch.com>
2016-08-05 14:09:39 +1000

    fdgg

--- src/TechTest01/TechTest01.Tests/Controllers/ProductsControllerTest.cs
+++ src/TechTest01/TechTest01.Tests/Controllers/ProductsControllerTest.cs
@@ -51,7 +51,7 @@ namespace WebApplication1.Tests.Controllers
         [TestMethod]
         public void Index()
         {
-            // Arrange
+            // Arrange  xx
             ProductsController controller = new ProductsController(_productService, _logManager);
 
             // Act
